3d0bac4ddcac3a3f234678acd5a5869a15fde8e3
Brian Carlston <Brian Carlston>
2015-12-15 00:25:19 -0500

    Added polylines to the map that take in a list lat longs.

---
+++ app/src/main/java/com/csitguys/happierhiking/HikeList.java
@@ -0,0 +1,11 @@
+package com.csitguys.happierhiking;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by david on 11/9/15.
+ */
+public class HikeList {
+    ArrayList<LatLngList> list = new ArrayList<>();
+}
---
+++ app/src/main/java/com/csitguys/happierhiking/LatLngList.java
@@ -0,0 +1,8 @@
+package com.csitguys.happierhiking;
+
+import com.google.android.gms.maps.model.LatLng;
+import java.util.ArrayList;
+
+public class LatLngList {
+    ArrayList<LatLng> list = new ArrayList<>();
+}
