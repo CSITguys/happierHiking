5ba08ecfa865ca7615bcd942f6f5fecb3a21856d
ppowell0015 <ppowell@csumb.edu>
2015-12-17 00:31:47 -0800

    Update LatLngList.java

--- app/src/main/java/com/csitguys/happierhiking/LatLngList.java
+++ app/src/main/java/com/csitguys/happierhiking/LatLngList.java
@@ -3,6 +3,8 @@ package com.csitguys.happierhiking;
 
 import java.util.ArrayList;
 
+//This page puts all lat's from hikes into an array to be used on the map
+
 public class LatLngList {
     ArrayList<LatLong> list = new ArrayList<>();
 }
