d2cebc1ef803462eadba111db21012bca6840628
ppowell0015 <ppowell@csumb.edu>
2015-12-17 00:34:04 -0800

    Update User.java

--- app/src/main/java/com/csitguys/happierhiking/User.java
+++ app/src/main/java/com/csitguys/happierhiking/User.java
@@ -3,6 +3,9 @@ package com.csitguys.happierhiking;
 /**
  * Created by Brian on 12/11/2015.
  */
+ 
+ //This class helps the application connect to the user table on the server REQUIREMENT #4
+ 
 public class User {
 
         public int id;
