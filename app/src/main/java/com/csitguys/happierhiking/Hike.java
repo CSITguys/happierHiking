1b8cfa2441190d93dc9d118cb0b73c73f4d2407f
ppowell0015 <ppowell@csumb.edu>
2015-12-17 00:29:33 -0800

    Update Hike.java

--- app/src/main/java/com/csitguys/happierhiking/Hike.java
+++ app/src/main/java/com/csitguys/happierhiking/Hike.java
@@ -5,6 +5,8 @@ import com.google.android.gms.maps.model.LatLng;
 /**
  * Created by Brian on 12/15/2015.
  */
+ 
+ //To help application connect to hike table in database REQUIREMENT #4
 public class Hike {
     int id;
     String name;
